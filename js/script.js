7daee6a3c953c6fcf4aba0f7125e65bda546dc32
Andrea <grassiandrea95@gmail.com>
2020-12-09 18:14:07 +0100

    nav social-account completa

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 var header= new Vue({      //per l'header
   el:'#header',
   data:{
-    socials:['fab fa-facebook-f','fab fa-twitter','fab fa-instagram','fab fa-youtube']
+    socials:[   // icone per social
+      {social:'fab fa-facebook-f', link:'https://it-it.facebook.com/'},
+      {social:'fab fa-twitter', link:'https://twitter.com/login?lang=it'},
+      {social:'fab fa-instagram', link:'https://www.instagram.com/'},
+      {social:'fab fa-youtube', link:'https://www.youtube.com/?hl=it&gl=IT'}
+    ],
 
+    profilo:[
+      {str:'Shopping Cart'},
+      {str:'My Account', dropdown:'fas fa-angle-down'},
+      {cart:'fas fa-shopping-cart',str:'CART',dropdown:'fas fa-angle-down'}
+    ]
   }
 });
+
+
+
+
+
 
 var footer= new Vue({   // per il footer
   el:'#footer',
